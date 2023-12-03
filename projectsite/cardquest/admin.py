e3c05605f60c8302c82cea0b0d6d9f5f88f761fb
kenlorenz <kenlorenz420@gmail.com>
2023-12-03 13:08:39 +0800

    added menus

--- projectsite/cardquest/admin.py
+++ projectsite/cardquest/admin.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from .models import PokemonCard, Trainer
+from .models import PokemonCard, Trainer, Collection
 
 # Register your models here.
 # admin.site.register(PokemonCard)
 
+# add admin menus
+
+@admin.register(Collection)
+
 @admin.register(PokemonCard)
 
 @admin.register(Trainer)
+
 class PokemonAdmin(admin.ModelAdmin):
     list_display = ("name", "rarity")
     search_fields = ("name",)
