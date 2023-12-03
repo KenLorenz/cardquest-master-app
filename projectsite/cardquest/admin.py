b1776d49fb6ecaa917feccf793a40e6b8146b38b
kenlorenz <kenlorenz420@gmail.com>
2023-12-03 09:37:35 +0800

    preparing for second attempt

--- projectsite/cardquest/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import PokemonCard
-
-@admin.register(PokemonCard)
-class PokemonAdmin(admin.ModelAdmin):
-    list_display = ("name", "rarity")
-    search_fields = ("name",)
