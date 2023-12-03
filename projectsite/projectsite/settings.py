69b0d9f957d0aded36eae20f8c2f6591d7d27bc9
kenlorenz <kenlorenz420@gmail.com>
2023-12-03 08:40:50 +0800

    re-do-ing the whole thing

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -23,9 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-cg^q4bqmna=fa$2yut6z%8gvb3mo*t2(w5q)x11sf68&v4d3ey'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = ['http://ren122846.pythonanywhere.com/']
+ALLOWED_HOSTS = ['http://ren122846.pythonanywhere.com/',
+                 '127.0.0.1',
+                 ]
 
 
 # Application definition
@@ -116,8 +118,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '../staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = './static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '../staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
