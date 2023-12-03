50e9bffe6c08309ac224e1f4d43c9bf7507dfcee
kenlorenz <kenlorenz420@gmail.com>
2023-12-03 12:14:33 +0800

    added more stuff

--- projectsite/cardquest/views.py
+++ projectsite/cardquest/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from cardquest.models import PokemonCard, Trainer
 
 # Create your views here.
+
+class TrainerList(ListView):
+    model = Trainer
+    context_object_name = 'trainer'
+    template_name = 'trainers.html'
+    paginate_by = 15
+
+class HomePageView(ListView):
+    model = PokemonCard
+    context_object_name = 'home'
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
