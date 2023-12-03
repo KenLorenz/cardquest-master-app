8765073f37ecf4e3ce1f7c9f32a161f5404a73b0
kenlorenz <kenlorenz420@gmail.com>
2023-12-03 14:16:05 +0800

    test

--- projectsite/cardquest/admin.py
+++ projectsite/cardquest/admin.py
@@ -15,3 +15,11 @@ from .models import PokemonCard, Trainer, Collection
 class PokemonAdmin(admin.ModelAdmin):
     list_display = ("name", "rarity")
     search_fields = ("name",)
+
+class TrainerAdmin(admin.ModelAdmin):
+    list_display = ("name", "birthdate")
+    search_fields = ("name",)
+    
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ("card", "collection_date")
+    search_fields = ("name",)
