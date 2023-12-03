47476c9d28216df8f7dfdd869a1368b0d544e52b
kenlorenz <kenlorenz420@gmail.com>
2023-12-03 12:18:15 +0800

    added more stuff

---
+++ projectsite/cardquest/url.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from cardquest.views import HomePageView, TrainerList
+from cardquest import views
+
+urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('', views.HomePageView.as_view(), name='home'),
+        path('trainer_list', TrainerList.as_view(), name='trainer-list'),
+        ]
