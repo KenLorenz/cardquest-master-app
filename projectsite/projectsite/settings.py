0e2ee3376bea147f3654a44cee1875e140f53ced
kenlorenz <kenlorenz420@gmail.com>
2023-12-03 10:46:11 +0800

    preparation for pythonanywhere

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-))he5&848hbece+$%_=pgzs**=0yp+wb4fqm^zlx33^e9ns(b-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['127.0.0.1']
 
 # Application definition
 
