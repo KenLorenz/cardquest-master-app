777de028892f4a0189f514b6cda1fc997b6777ce
KenLorenz <kenlorenz420@gmail.com>
2023-12-05 04:56:56 +0800

    fix urls/url

--- projectsite/cardquest/url.py
+++ projectsite/cardquest/url.py
@@ -1,9 +1,10 @@
+from django.contrib import admin
 from django.urls import path
 from cardquest.views import HomePageView, TrainerList
 from cardquest import views
 
 urlpatterns = [
-        path('admin/', admin.site.urls),
-        path('', views.HomePageView.as_view(), name='base'),
-        path('trainer_list/', TrainerList.as_view(), name='trainer-list'),
-        ]
+        path('', admin.site.urls),
+        # path('', views.HomePageView.as_view(), name='base'),
+        # path('trainer_list/', TrainerList.as_view(), name='trainer-list'),
+]
--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -21,7 +21,7 @@ from cardquest.views import HomePageView, TrainerList
 from cardquest import views
 
 urlpatterns = [
-        path('admin/', admin.site.urls),
-        path('', views.HomePageView.as_view(), name='home'),
-        path('trainer_list', TrainerList.as_view(), name='trainer-list'),
-        ]
+        path('', admin.site.urls),
+        # path('', views.HomePageView.as_view(), name='home'),
+        # path('trainer_list', TrainerList.as_view(), name='trainer-list'),
+]
