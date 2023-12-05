f6491b7993e335e98d4c21ce2a7777dc37d6fb53
KenLorenz <kenlorenz420@gmail.com>
2023-12-05 19:28:16 +0800

    fix trainers (incomplete)

--- projectsite/cardquest/url.py
+++ projectsite/cardquest/url.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+""" from django.contrib import admin
 from django.urls import path
 from cardquest.views import HomePageView, TrainerList, CollectionList
 from cardquest import views
@@ -6,6 +6,5 @@ from cardquest import views
 urlpatterns = [
         path('admin/', admin.site.urls),
         path('', views.HomePageView.as_view(), name='home'),
-        # path('trainers_list', views.TrainerList.as_view(), name='trainer-list'),
-        # path('collections_list', views.CollectionList.as_view(), name='collection-list'),
 ]
+ """
